2cf146960a16a704728dc10baab1ddbb162054fa
Akoly Vongdala <akolyv@uw.edu>
2021-05-25 13:31:09 -0700

    Create new project and update github pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
+import { useFetch } from "./hooks/useFetch";
+
 
 const viewHeight = 500;
 const viewWidth = 500;
 
 const App = () => {
     return (
-        <svg style={{ border: "5px solid red", width:viewWidth, height:viewHeight}}>
-            <circle cx={20} cy={20} r="5"></circle>
-            <rect x={200} y={200} width={10} height={10} fill="pink"/>
-            <rect x={212} y={200} width={10} height={10} fill="black"/>
-            <rect x={224} y={200} width={10} height={10} fill="black"/>
-            <line x1={0} y1={viewHeight} x2={150} y2={100} stroke="orange"/>
-            <text x="50" y="50" style={{ font: "italic 16px" }}>
-                ummmmm add something lol
-            </text>
-            <text> adding another thing</text>
-        </svg>
+        <h1>INFO 474 Final Project </h1>
     );
     
 };
