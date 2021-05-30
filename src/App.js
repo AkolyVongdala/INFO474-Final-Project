8996cbac7c56d6e85b37769ed6fc37e1b8e24aa0
anie0521 <anie0521@uw.edu>
2021-05-30 15:22:50 -0700

    changed col names

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const viewHeight = 500;
 const viewWidth = 500;
 
 const App = () => {
+    const [data, loading] = useFetch(
+        "https://raw.githubusercontent.com/AkolyVongdala/INFO474-Final-Project/main/data/Info474_FinalData.csv"
+    );
+
+    let formatData = data.map(function (d) {
+    })
+
     return (
         <h1>INFO 474 Final Project </h1>
     );
